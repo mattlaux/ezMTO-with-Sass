c74fa5f0425c94b4e271fb968f811ddb5a609194
drattylight <mlaux6@outlook.com>
2022-01-28 13:03:21 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+ 
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: true
+    }));
+});
+
+gulp.task('default', function() {
+  return gulp.watch('.', gulp.series(['webserver']));
+});
